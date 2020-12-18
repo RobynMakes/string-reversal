c9ad601931216d1d48554b3d8eddafa3edf3630b
Robyn Gibson <robynmakes@gmail.com>
2020-12-18 13:18:38 -0600

    Working Base Program

--- src/robyn/stringReversal/Main.java
+++ src/robyn/stringReversal/Main.java
@@ -8,13 +8,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String stringInput;
-        ArrayList<Character> stringArray = new ArrayList<Character>();
+        ArrayList<Character> stringArray = new ArrayList<>();
+        ArrayList<Character> reversedArray = new ArrayList<>();
 
         System.out.println("Please Enter A String: ");
         stringInput = input.next();
 
+        input.close();
+
         for(int i = 0; i != stringInput.length() ; i++){
-            stringArray.add((char) stringInput.indexOf(i));
+            stringArray.add((stringInput.charAt(i)));
         }
+
+        for(int i = stringArray.size()-1; i >= 0; i--){
+            reversedArray.add(stringArray.get(i));
+        }
+
+        for(int i = 0; i != reversedArray.size(); i++){
+            System.out.print(reversedArray.get(i));
+        }
+
     }
 }
