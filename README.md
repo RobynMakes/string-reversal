e86691fd1ee1d2b743cf85de26d20a484342ecb2
Robyn Gibson <robynmakes@gmail.com>
2020-12-18 12:23:22 -0600

    Started on the base program

---
+++ README.md
@@ -0,0 +1,20 @@
+# string-reversal [![NPM version](https://badge.fury.io/js/string-reversal.svg)](https://npmjs.org/package/string-reversal) [![Build Status](https://travis-ci.org/RobynMakes/string-reversal.svg?branch=master)](https://travis-ci.org/RobynMakes/string-reversal)
+
+> A simple program that will reverse a string given to it.
+
+## Installation
+
+```sh
+$ npm install --save string-reversal
+```
+
+## Usage
+
+```js
+var stringReversal = require('string-reversal');
+stringReversal();
+```
+
+## License
+
+MIT © [Robyn Gibson](https://github.com/RobynMakes)
--- src/robyn/stringReversal/Main.java
+++ src/robyn/stringReversal/Main.java
@@ -1,11 +1,20 @@
 package robyn.stringReversal;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        String stringInput;
+        ArrayList<Character> stringArray = new ArrayList<Character>();
+
+        System.out.println("Please Enter A String: ");
+        stringInput = input.next();
+
+        for(int i = 0; i != stringInput.length() ; i++){
+            stringArray.add((char) stringInput.indexOf(i));
+        }
     }
 }
